19910807260889cabec0d65884337699592025d7
hanfling <13674464+hanfling@users.noreply.github.com>
2022-11-11 07:46:52 +0100

    Added TriangleNoVertexAttributes, which omits any vertex attributes and instead stores the vertex data inside the vs and selects it based on gl_VertexID.

---
+++ TriangleNoVertexAttributes/VertexShader.glsl
@@ -0,0 +1,40 @@
+#version 450
+
+#extension GL_ARB_separate_shader_objects : enable
+#extension GL_ARB_shading_language_420pack : enable
+
+layout (binding = 0) uniform UBO 
+{
+	mat4 projectionMatrix;
+	mat4 modelMatrix;
+	mat4 viewMatrix;
+} ubo;
+
+layout (location = 0) out vec3 outColor;
+
+out gl_PerVertex 
+{
+    vec4 gl_Position;   
+};
+
+void main() 
+{
+	const vec3 vPos[3] =
+	{
+		vec3( 1.0,-1.0, 0.0 ),
+		vec3(-1.0,-1.0, 0.0 ),
+		vec3( 0.0, 1.0, 0.0 )
+	};
+	const vec3 vCol[3] =
+	{
+		vec3( 1.0, 0.0, 0.0 ),
+		vec3( 0.0, 1.0, 0.0 ),
+		vec3( 0.0, 0.0, 1.0 )
+	};
+
+	const vec3 inPos   = vPos[gl_VertexID%3];
+	const vec3 inColor = vCol[gl_VertexID%3];
+
+	outColor = inColor;
+	gl_Position = ubo.projectionMatrix * ubo.viewMatrix * ubo.modelMatrix * vec4(inPos.xyz, 1.0);
+}
